dd786f8db6be7d24b942c168dc59c04ae026fa78
Santiago Algozino <santiago.algozino@satellogic.com>
2022-03-06 11:36:27 -0300

    replace latestStakes and latestDisputes with MUI

---
+++ packages/react-app/src/components/LatestStakes.js
@@ -0,0 +1,81 @@
+import { useQuery } from "@apollo/client";
+import { wei2eth } from "../scripts/utils";
+import { Person } from '@mui/icons-material/';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { LASTSTAKES } from "../queries/stakes";
+import { Grid, List, ListItem, ListItemAvatar, ListItemText, Skeleton, Avatar, Chip, Typography, Paper } from "@mui/material";
+
+export default function LatestStakes() {
+
+
+  function createStakeItem(stake, i) {
+    return (
+      <ListItem key={i} secondaryAction={
+        <Chip label={stake.stake.toFixed(0) + ' PNK'} />
+      }>
+        <ListItemAvatar>
+          <Avatar>
+            <Person />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary={<Link to={"/profile/" + stake.juror}>{stake.juror}</Link>}
+          secondary={"Court: " + stake.subcourtId}>
+        </ListItemText>
+      </ListItem>
+    )
+  }
+
+  const [stakesItems, setStakeItems] = useState(() => <Skeleton />);
+
+
+  const handleStakeData = (data) => {
+    parseStakeData(data.stakeSets);
+  }
+
+
+  async function parseStakeData(stakesData) {
+    if (stakesData === undefined) return []
+    let newStakesData = [];
+    stakesData.forEach((stake) => {
+      newStakesData.push(
+        {
+          'juror': stake.address.id,
+          'subcourtId': stake.subcourtID,
+          'stake': wei2eth(stake.stake),
+        }
+      );
+    });
+
+    const items = []
+    newStakesData.forEach((stake, i) => {
+      const item = createStakeItem(stake, i)
+      items.push(item);
+    })
+    setStakeItems(items)
+  }
+
+  const { error, foo, loading } = useQuery(LASTSTAKES, { onCompleted: handleStakeData });
+
+  if (error) { console.log(error) }
+  return (
+    <Grid item xs={12} md={6}>
+      <Paper
+        elevation={3} 
+        sx={{
+          p: 2,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div" align="center">
+          Latest Stakes
+        </Typography>
+        <List dense={true}>
+          {stakesItems}
+        </List>
+      </Paper>
+    </Grid >
+  )
+}
---
+++ packages/react-app/src/components/latestDisputes.js
@@ -0,0 +1,94 @@
+import { useQuery } from "@apollo/client";
+import { Table, TableBody, TableCell, TableHead, TableRow, Typography, Grid, Paper, Chip } from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { LASTDISPUTES } from "../queries/disputes";
+import { getCourtName } from "../scripts/utils";
+
+
+
+export default function LatestDisputes() {
+  const buttonColors = {'evidence':'#75a832', 'vote':'#fffc5e', 'appeal':'#fa9420', 'excecution':'#6b75ff'}
+
+  function createTableRow(dispute, id) {
+    return (
+      <TableRow key={id}>
+        <TableCell><Link to={"/cases/" + dispute.id}> {dispute.id} </Link></TableCell>
+        <TableCell><Typography>{dispute.courtName}</Typography></TableCell>
+        <TableCell><Typography>{dispute.jurorsInvolved}</Typography></TableCell>
+        <TableCell><Chip variant="outlined" label={dispute.period} style={{backgroundColor:buttonColors[dispute.period]}}  /></TableCell>
+      </TableRow>
+    )
+  }
+
+  const [disputesTableRows, setdisputesTableRows] = useState(() => []);
+
+  const handleDisputesData = (data) => {
+    parseDisputeData(data.disputes);
+  }
+
+  async function parseDisputeData(disputesData) {
+    if (disputesData === undefined) return []
+    let newDisputesData = [];
+    let promises = [];
+    disputesData.forEach((dispute) => {
+      promises.push(dispute.subcourtID.policy ? getCourtName(dispute.subcourtID.policy.policy) : dispute.subcourtID.id);
+    });
+    const courtNames = await Promise.all(promises).then((values) => {
+      return values;
+    })
+    disputesData.forEach((dispute, i) => {
+      newDisputesData.push(
+        {
+          'id': dispute.id,
+          'subcourtID': dispute.subcourtID.id,
+          'courtName': courtNames[i],
+          'jurorsInvolved': Object.keys(dispute.jurorsInvolved).length,
+          'period': dispute.period
+        }
+      );
+    });
+    
+    const tableRows = []
+    newDisputesData.forEach((dispute, id) => {
+      const tableRow = createTableRow(dispute, id)
+      tableRows.push(tableRow);
+    })
+    setdisputesTableRows(tableRows)
+  }
+
+  const { error, foo, loading } = useQuery(LASTDISPUTES, { onCompleted: handleDisputesData });
+
+  if (error) {console.log(error)};
+
+  return (
+    <Grid item xs={12} md={6}>
+      <Paper
+        elevation={3}
+        sx={{
+          p: 2,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div" align="center">
+          Latests Disputes
+        </Typography>
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Dispute ID</TableCell>
+              <TableCell>Court</TableCell>
+              <TableCell># Jurors</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {disputesTableRows}
+          </TableBody>
+        </Table>
+      </Paper>
+    </Grid >
+
+  )
+}
