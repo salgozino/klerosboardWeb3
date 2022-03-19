71b836734042c4100347e4f86ce3faaf8e7114b8
Santiago Algozino <santiago.algozino@satellogic.com>
2022-03-19 17:47:10 -0300

    create chainSelector page. clean images

---
+++ packages/react-app/src/pages/ChainSelector.jsx
@@ -0,0 +1,42 @@
+import { Card, CardContent, CardMedia, CardActions, Container, Grid, Typography, Button } from '@mui/material'
+import { Link as LinkRouter } from 'react-router-dom';
+import React from 'react'
+
+export default function ChainSelector() {
+  return (
+    <Container>
+      <Grid container sx={{ marginTop: '50px' }}>
+        <Grid item xs={6} md={6}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              minHeight="140px"
+              image="/ethereum_logo.png"
+              alt="Gnosis"
+            />
+            <CardContent>
+              <CardActions sx={{ justifyContent: "center" }}>
+                <LinkRouter to="/1"><Button size="medium">Mainnet</Button></LinkRouter>
+              </CardActions>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6} md={6}>
+          <Card sx={{ maxWidth: 345}}>
+            <CardMedia
+              component="img"
+              height="345px"
+              image="/gnosis_avatar_green_white.png"
+              alt="Gnosis"
+            />
+            <CardContent>
+              <CardActions sx={{ justifyContent: "center" }}>
+                <LinkRouter to="/100"><Button size="medium">Gnosis</Button></LinkRouter>
+              </CardActions>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
